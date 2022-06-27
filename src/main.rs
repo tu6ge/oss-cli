8fa1d754c2b4b3eef0c372afe486108a1f6d3579
tu6ge <772364230@qq.com>
2022-06-27 20:59:48 +0800

    存一下

---
+++ src/main.rs
@@ -0,0 +1,142 @@
+use clap::{Args,Parser,Subcommand};
+use crossterm::style::ContentStyle;
+use reqwest::{header::{HeaderMap, HeaderValue},Body};
+use std::{path::PathBuf, io::{Read, self, Write}};
+use input_stream::InputStream;
+use tokio::fs::File;
+use tokio_util::codec::{BytesCodec, FramedRead};
+use futures::stream::TryStreamExt;
+use ml_progress::progress;
+use std::fs;
+
+
+use aliyun_oss_client::{client::{Client, ReqeustHandler}, errors::{OssResult, OssError}, auth::VERB};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[clap(arg_required_else_help = true)]
+    Up{
+        /// 本地文件路径
+        #[clap(value_parser)]
+        source: PathBuf,
+
+        /// OSS 文件 key
+        #[clap(value_parser)]
+        target: String,
+
+        #[clap(short, long, value_parser)]
+        key: Option<String>,
+        #[clap(short, long, value_parser)]
+        secret: Option<String>,
+        #[clap(short, long, value_parser)]
+        endpoint: Option<String>,
+        #[clap(short, long, value_parser)]
+        bucket: Option<String>,
+    },
+    #[clap(arg_required_else_help = true)]
+    Down{
+        /// OSS 文件 key
+        #[clap(value_parser)]
+        source: String,
+
+        /// 本地文件路径
+        #[clap(value_parser)]
+        target: PathBuf,
+    }
+}
+
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let args = Cli::parse();
+
+    let mut stdout = io::stdout().lock();
+
+            stdout.write_all(b"hello world").unwrap();
+
+    dotenv().ok();
+
+    match args.command {
+        Commands::Up{
+            source,
+            target,
+            key,
+            secret,
+            endpoint,
+            bucket
+        } => {
+            let key = key.unwrap_or(env::var("ALIYUN_KEY_ID").unwrap());
+            let secret = secret.unwrap_or(env::var("ALIYUN_KEY_SECRET").unwrap());
+            let endpoint = endpoint.unwrap_or(env::var("ALIYUN_ENDPOINT").unwrap());
+            let bucket = bucket.unwrap_or(env::var("ALIYUN_BUCKET").unwrap());
+            
+            // let client = aliyun_oss_client::client(&key,&secret, &endpoint, &bucket);
+            //     let res = client.put_file(source, target.as_str()).await;
+            //     println!("res: {res:?}");
+            
+            // let paths = fs::read_dir(source).unwrap();
+
+            // for path in paths {
+            //     //let f = fs::File::open(path.unwrap().path());
+            //     println!("Name: {:?}", path.unwrap().file_name())
+            // }
+
+            
+
+        },
+        Commands::Down{ source, target} => {
+
+        }
+    }
+
+}
+
+async fn put_content_process(client: &Client<'_>) -> OssResult<String>{
+    let file_path = "9AB932LY.jpeg";
+    let mut file_size = std::fs::File::open(file_path)?;
+    let file = File::open(file_path).await?;
+    let mut file_content = Vec::new();
+    file_size
+    .read_to_end(&mut file_content)?;
+
+    let mut url = client.get_bucket_url()?;
+    url.set_path("abc.png");
+
+    let mut headers = HeaderMap::new();
+    let content_length = file_content.len().to_string();
+    headers.insert(
+    "Content-Length", 
+    HeaderValue::from_str(&content_length).map_err(|_| OssError::Input("Content-Length parse error".to_string()))?);
+
+    headers.insert(
+    "Content-Type", 
+    "jpeg".parse().unwrap());
+
+    let response = client.builder(VERB::PUT, &url, Some(headers), None).await?
+    .body(file_to_body(file));
+
+    let content = response.send().await?.handle_error()?;
+
+    let result = content.headers().get("ETag")
+    .ok_or(OssError::Input("get Etag error".to_string()))?
+    .to_str().map_err(|_| OssError::Input("ETag parse error".to_string()))?;
+
+    Ok(result.to_string())
+}
+
+fn file_to_body(file: File) -> Body {
+    let stream = FramedRead::new(file, BytesCodec::new());
+    let body = Body::wrap_stream(stream);
+    body
+}
